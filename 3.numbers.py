1c80e0e642212c6fd6cdbe10625246b87140f122
Rishik Chakravarty <rishikrc3@gmail.com>
2024-02-24 13:15:57 +0530

    3_exercise_done

---
+++ 3.numbers.py
@@ -0,0 +1,20 @@
+length = 92
+width = 48.8
+area = length*width
+
+print(area)
+
+packets = 9
+packetCost = 1.49
+totalCost = packets*packetCost
+paid = 20
+
+remainingCost = abs(totalCost-paid)
+print(remainingCost)
+
+tileArea=5.5*5.5
+totalPrice = 500*tileArea
+print(totalPrice)
+
+binaryRepresentation = bin(17)
+print(binaryRepresentation)
