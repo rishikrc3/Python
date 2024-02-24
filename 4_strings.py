24dc1fc4859836344d544ff57500455411c09546
Rishik Chakravarty <rishikrc3@gmail.com>
2024-02-24 13:38:32 +0530

    strings_

---
+++ 4_strings.py
@@ -0,0 +1,22 @@
+street= "Kundarali"
+city  = "Kolkata"
+country = "India"
+ans = f"I am from {street}, in {city}, which is obviously in {country}"
+
+print(street+city+country)
+print(ans)
+
+sen="Earth revolves around the Sun"
+print(sen[-3:])
+print(sen[6:13])
+
+x= "beans"
+y= "roti"
+
+ans2=f"I eat {x} and {y} for dinner everyday"
+print(ans2)
+
+s='maine 200 banana khaye'
+
+s_new = s.replace("200 banana","10 samose")
+print(s_new)
